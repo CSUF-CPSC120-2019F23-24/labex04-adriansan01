664a86c79ef4e5a4d3e086dce6b3ac3eabcd39d7
adriansan01 <adriansan01@csu.fullerton.edu>
2019-09-14 14:34:29 -0700

    Sales Predictions

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,17 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  //Show the total sales
+  const long TOTAL_SALE = 2103419277;
+  const long long TODAYS_SALES = TOTAL_SALE * 1.18;
+
+
+  std::cout << "Last year's sales were $" << TOTAL_SALE <<std::endl;
+  std::cout << "This year's sales predictions: $" <<TODAYS_SALES << std::endl;
+
+
+return 0;
+}
